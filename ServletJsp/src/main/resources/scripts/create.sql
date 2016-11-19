904141fb7acfb132f7f14642add122ae4c159f1f
mpl96 <mpl_96@hotmail.es>
2016-11-19 16:22:47 +0100

    hemos modificado las consultas para las tablas
    Signed-off-by: mpl96 <mpl_96@hotmail.es>

--- ServletJsp/src/main/resources/scripts/create.sql
+++ ServletJsp/src/main/resources/scripts/create.sql
@@ -1,7 +1,10 @@
-create table IF NOT EXISTS PAISES(
-	PAIS VARCHAR(50)
-	);
-	
 create table IF NOT EXISTS IDIOMAS(
-	IDIOMA VARCHAR(50)
+	idioma varchar(25) PRIMARY KEY
+);
+
+create table IF NOT EXISTS PAISES(
+	idPais int PRIMARY KEY,
+	pais varchar(25),
+	idIoma varchar(25),
+	FOREIGN KEY(idioma) REFERENCES IDIOMAS(idioma)
 );
